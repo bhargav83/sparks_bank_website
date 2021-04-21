58a3c6a647f66a0617472701ae871c8cfdd612fa
bhargav83 <82281693+bhargav83@users.noreply.github.com>
2021-04-21 12:31:46 +0530

    Add files via upload

---
+++ index.php
@@ -0,0 +1,59 @@
+<html>
+<head>
+    <title> A Basic Banking System</title>
+    <link rel="shortcut icon" href="images/logo.jpeg">
+   
+	 <link rel="stylesheet" href="index_button.css">
+	<style>
+	button{
+		background-color:#8cbed6;
+	}
+	body
+	{
+		text-align:center;
+	}
+	body{
+		background-image:url("images/b9.jpg");
+		background-position: center;
+		background-repeat: no-repeat;
+		background-size: cover;
+	}
+	</style>
+</head>
+<body >
+	<div id="header">
+       <br>
+       <h1 style=" font-family:Agency FB; font-size: 70px; color:#03fcc2;text-shadow: 2px 2px black;"> The Sparks Bank </h1>
+       <h2 style=" font-family:Agency FB; font-size: 55px;color:#5203fc;text-shadow: 2px 2px black;">&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp; A Basic Banking System </h2>
+    </div>
+        <div id="section">
+            <table>
+                <tr></tr>
+                <tr><br>
+				<a href="getdetail.php">
+               <button class="primary_btn" id="button "type="button" href="getdetail.php">View users</button>
+                </a>
+               
+                </tr>
+                <tr>        
+               <br> <br> <br>
+			   <a href="transfer.php">
+			   <button class="primary_btn" id="button" type="button">Credit Transfer</button>
+               </a>
+               
+               </tr>
+			   
+			    <tr>        
+               <br> <br> <br>
+			   <a href="transaction.php">
+			   <button class="primary_btn" id="button" type="button">All Transaction</button>
+               </a>
+            </table>
+    </div>
+<footer>
+<footer>
+  <p style="color:white;">Copyright @ 2021 A Basic Banking System by Sai bhargav kaipa</p>
+  <p><a href="mailto:saibhargavkaipao2@gmail.com">saibhargavkaipao2@gmail.com</a></p>
+</footer>
+</body>
+</html>
